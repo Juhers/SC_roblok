c79158b673be6426aedf782a5767879e5d79e910
ZUHA <akunprank985@gmail.com>
2026-06-27 04:17:21 +0800

    adsdsad

--- Find_FarmPlot.lua
+++ Find_FarmPlot.lua
@@ -228,4 +228,4 @@ player.CharacterAdded:Connect(function(newChar)
     notify("🔄 Respawn", "Character baru terdeteksi", 3)
 end)
 
-notify("🚀 Script Loaded", "Deep Underground + Adaptive Crawl\nTekan F untuk memulai", 6)
+notify("🚀 Script Loaded", "Deep Underground + Adaptive Crawl\nTekan O untuk memulai", 6)
