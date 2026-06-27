38130a8c29b0da004393bf9dbe803a6f13961471
ZUHA <akunprank985@gmail.com>
2026-06-27 12:57:10 +0800

    adadad

--- Find_FarmPlot.lua
+++ Find_FarmPlot.lua
@@ -347,7 +347,7 @@ end
 -- ================== HOTKEY ==================
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
     if gameProcessed then return end
-    if input.KeyCode == Enum.KeyCode.J then
+    if input.KeyCode == Enum.KeyCode.G then
         toggleLoop()
     end
 end)
